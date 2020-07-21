b92384941964beb390eb7e46f25615e3274390ef
gabriel <gabo.lazos@gmail.com>
2020-07-21 12:09:35 -0500

    changes

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_lmiz8r3G3', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7le3ds260uecj4tjg93o0toh82', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1b13q32ou7mbhi88lmmhlj69ur', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
