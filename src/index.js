efeaf66f0143a0247a7d4c563e407c590d3b9e55
gabriel <gabo.lazos@gmail.com>
2020-07-21 12:52:05 -0500

    adding aws-amplify

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from "aws-amplify"
+import config from "./config.json"
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +12,15 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  }
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
