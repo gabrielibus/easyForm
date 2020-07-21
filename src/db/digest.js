54ae28b22c877d8dccb3271acfa948599512760b
gabriel <gabo.lazos@gmail.com>
2020-07-21 11:13:45 -0500

    adding Cognito

--- src/db/digest.js
+++ src/db/digest.js
@@ -1,27 +1,57 @@
-var fetch = require("node-fetch")
+var axios = require("axios");
+var fetch = require("node-fetch");
 
 const putData = (data) => {
-  var url = "https://npr11n9l34.execute-api.us-east-1.amazonaws.com/beta/indutramitesPut"
+  var url =
+    "https://npr11n9l34.execute-api.us-east-1.amazonaws.com/beta/indutramitesPut";
 
-  var myHeaders = new Headers()
-  myHeaders.append("Content-Type", "application/json")
-  myHeaders.append("x-api-key", "gZUfNjgwbI3Gjn2ovckj3ciGsvXzhF2af9v4RMe2")
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("x-api-key", "gZUfNjgwbI3Gjn2ovckj3ciGsvXzhF2af9v4RMe2");
 
   // var raw = JSON.stringify(data)
-  var raw = JSON.stringify({"id":"11222","productName":"test from frontend"});
+  var data = JSON.stringify({ id: "11222", productName: "test from frontend" });
 
+  var config = {
+    method: "post",
+    url:
+      "https://npr11n9l34.execute-api.us-east-1.amazonaws.com/beta/indutramitesPut",
+    headers: {
+      "x-api-key": "gZUfNjgwbI3Gjn2ovckj3ciGsvXzhF2af9v4RMe2",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+    data: data,
+  };
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
-  var requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: raw,
-    redirect: "follow",
-  }
-  console.log('working')
-  fetch(url, requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log("error", error))
-}
+  // var requestOptions = {
+  //   method: "POST",
+  //   headers: myHeaders,
+  //   body: raw,
+  //   redirect: "follow",
+  // }
+  // console.log('working')
+  // fetch(url, requestOptions)
+  //   .then((response) => response.text())
+  //   .then((result) => console.log(result))
+  //   .catch((error) => console.log("error", error))
+};
 
-export default putData
+export default putData;
+
+// var data = JSON.stringify({ id: "00011", productName: "aguacate4s" });
+
+// axios(config)
+//   .then(function (response) {
+//     console.log(JSON.stringify(response.data));
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
---
+++ src/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_lmiz8r3G3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7le3ds260uecj4tjg93o0toh82', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://indutramites.d1qjw51t9d9lnr.amplifyapp.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
