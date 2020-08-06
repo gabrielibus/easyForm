61d2756d335e98804efac7e2461f1ca4343a635b
gabriel <gabo.lazos@gmail.com>
2020-08-05 23:56:17 -0500

    linking to EC2

--- src/db/digest.js
+++ src/db/digest.js
@@ -14,7 +14,9 @@ const putData = (data) => {
   };
   // make API call with parameters and use promises to get response
   fetch(
-    "https://2noi9jhrr3.execute-api.us-east-1.amazonaws.com/dev",
+    // "http://localhost:3001/api/v1/post",
+    "http://52.91.153.228:3001/api/v1/post",
+    // "https://2noi9jhrr3.execute-api.us-east-1.amazonaws.com/dev",
     requestOptions
   )
     .then((response) => response.text())
