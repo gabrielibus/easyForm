c5196546434befe508b92ee973d76e5f440cd647
gabriel <gabo.lazos@gmail.com>
2020-08-11 16:35:20 -0500

    thowing to heroku

--- src/db/digest.js
+++ src/db/digest.js
@@ -44,11 +44,12 @@ const putData = (data) => {
     body: raw,
     redirect: "follow",
   };
-  
+
   // make API call with parameters and use promises to get response
   fetch(
     // "http://localhost:3001/api/v1/post",
-    "http://52.91.153.228:3001/api/v1/post",
+    // "http://52.91.153.228:3001/api/v1/post",
+    "https://infinite-escarpment-14960.herokuapp.com/",
     // "https://2noi9jhrr3.execute-api.us-east-1.amazonaws.com/dev",
     requestOptions
   )
